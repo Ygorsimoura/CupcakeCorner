0e642a5b22d5163421deacc37ee5a216693922f1
ygorsimoura <ygorsimoura01@gmail.com>
2023-11-18 17:25:43 -0300

    first commit: Implementing apple itunes API

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ygor Simoura on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
