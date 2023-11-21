bc658559170afb2ab19d5afe7f9d412c6328e3e0
ygorsimoura <ygorsimoura01@gmail.com>
2023-11-21 10:09:07 -0300

    Inicial Construction: Create Class Order and implemate a simple View to required the Cupcakes

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Ygor Simoura on 21/11/23.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView()
+}
---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Ygor Simoura on 21/11/23.
+//
+
+import Foundation
