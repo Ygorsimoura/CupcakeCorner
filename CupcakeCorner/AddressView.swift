acb2e5f91c87533cd5e26d2021c974c541ec7b42
ygorsimoura <ygorsimoura01@gmail.com>
2023-11-23 15:44:59 -0300

    changes: add a header in Adress View and create a new Ui to AdressView

--- CupcakeCorner/AddressView.swift
+++ CupcakeCorner/AddressView.swift
@@ -12,24 +12,35 @@ struct AddressView: View {
     @Bindable var order: Order
     
     var body: some View {
-            Form {
-                Section {
+            Form{
+                Section(header: Text("Where should we delivery you order?").padding()){
                     TextField("Name", text: $order.name)
                     TextField("Street Address", text: $order.streetAddress)
                     TextField("City", text: $order.city)
                     TextField("Zip", text: $order.zip)
                 }
-                
-                
-                Section {
-                    NavigationLink("Checkout") {
-                        CheckoutView(order: order)
-                    }
-                }
-                .disabled(order.hasValidAddress)
             }
             .navigationTitle("Delivery details")
             .navigationBarTitleDisplayMode(.inline)
+        
+        Section {
+            NavigationLink("Checkout") {
+                CheckoutView(order: order)
+            }
+            .frame(width: 332, height: 70)
+            .background{
+                if order.hasValidAddress {
+                    Color(.gray)
+                } else {
+                    Color(.cyan)
+                }
+            }
+            .font(.system(size: 20).bold())
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 18.0))
+        }
+        .disabled(order.hasValidAddress)
+        .padding(.bottom, 30)
     }
 }
 
